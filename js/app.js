a4785e1c59f5f175b437f1c6d16e7236f603b650
Hoodoo42 <create@hoodooverse.ca>
2022-07-16 09:51:40 -0600

    DOM manipulation warmup. working in troubleshooting the setAttribute and then an includes for the next step

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+let main = document.getElementById(`first_main`);
+main[`style`][`background`] = `lightblue`; 
+main[`style`][`fontFamily`] = `monospace`;
+
+let changeClass = document.getElementsByClassName(`text`);
+for(i=0; i<changeClass.length; i++){
+    changeClass[i][`innerHTML`] = `Data has been Overwritten with a link`;
+};
+
+// let images = document.querySelectorAll(`img`);
+// for(i=0; i<images.length; i++){
+// images.setAttribute(`src`, `https://images.unsplash.com/photo-1513775192371-1b9d33760c3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80`);
+// };
+
