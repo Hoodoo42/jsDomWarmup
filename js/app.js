920aa6523d15ce8115fdb65b2a38635e1592be33
Hoodoo42 <create@hoodooverse.ca>
2022-07-16 09:54:14 -0600

    finished the set.Attribute was missing the [i] to count through the images to change their src

--- js/app.js
+++ js/app.js
@@ -9,8 +9,9 @@ for(i=0; i<changeClass.length; i++){
     changeClass[i][`innerHTML`] = `Data has been Overwritten with a link`;
 };
 
-// let images = document.querySelectorAll(`img`);
-// for(i=0; i<images.length; i++){
-// images.setAttribute(`src`, `https://images.unsplash.com/photo-1513775192371-1b9d33760c3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80`);
-// };
+let images = document.querySelectorAll(`img`);
+for(i=0; i<images.length; i++){
+images[i].setAttribute(`src`, `https://images.unsplash.com/photo-1513775192371-1b9d33760c3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80`);
+};
+
 
